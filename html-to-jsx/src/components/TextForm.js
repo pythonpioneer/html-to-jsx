ccd2ce851a172782b224895210562977c95cfa28
Hritik <pypi.pioneer@gmail.com>
2023-07-23 21:10:34 +0530

    textarea work done

--- html-to-jsx/src/components/TextForm.js
+++ html-to-jsx/src/components/TextForm.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /**
  * This component is used as form.
  * @returns {JSX.Element} - A JSX element used to take input and show output
  */
 export default function TextForm() {
+
+    const [text, setText] = useState("<h1> Enter Your HTML Here </h1>");  // state varible for textarea
+    
+    // creating a funciton to convert html to jsx
+    const convertHtmlToJsx = 0;
+
+    // this method allows us to write on html-textarea
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+
     return (
         <>
             <div style={{ marginTop: '10px' }}>
                 <form>
                     <div class="row">
                         <div class="col">
-                            <textarea type="text" class="form-control" placeholder="Enter Html here" rows='22' style={{ width: '98%', marginLeft: '12px' }}></textarea>
+                            <textarea type="text" class="form-control" value={text} onChange={handleOnChange} rows='22' style={{ width: '98%', marginLeft: '12px' }}></textarea>
                         </div>
                         <div class="col">
-                            <textarea type="text" class="form-control" placeholder="JSX, comes here!!" rows='22' style={{ width: '98%' }}></textarea>
+                            <textarea type="text" class="form-control" value={''} placeholder="JSX, comes here!!" rows='22' style={{ width: '98%' }}></textarea>
                         </div>
                     </div>
                 </form>
@@ -22,7 +34,7 @@ export default function TextForm() {
 
             {/* adding button to convert html to jsx */}
             <div className='container'>
-                <button type="button" className="container btn btn-primary" style={{}}>Convert HTML to JSX</button>
+                <button type="button" className="container btn btn-primary" onClick={convertHtmlToJsx}>Convert HTML to JSX</button>
             </div>
         </>
     )
