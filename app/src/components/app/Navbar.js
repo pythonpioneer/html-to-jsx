8a343119a3f09ad22c9d280d806670d99b79e64f
Hritik <pypi.pioneer@gmail.com>
2023-12-02 01:58:55 +0530

    div to nav changed

--- app/src/components/app/Navbar.js
+++ app/src/components/app/Navbar.js
@@ -6,11 +6,11 @@ import './waveNavbar.css';
 export default function Navbar() {
     return (
         <>
-            <div className='spacer layer' style={{ width: '100%', height: '14vh' }}>
+            <nav className='spacer layer' style={{ width: '100%', height: '14vh' }}>
                 <div style={{ fontWeight: 'bold', fontSize: '25px', marginLeft: '2%', paddingTop: '1vh', color: 'white' }}>
                     HTML to JSX
                 </div>
-            </div>
+            </nav>
         </>
     )
 }
