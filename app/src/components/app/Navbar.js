1d1008a84f9428c4ccd3d2a3d137b8f6b4376284
Hritik <pypi.pioneer@gmail.com>
2023-12-02 01:35:33 +0530

    navigation bar added

---
+++ app/src/components/app/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './waveNavbar.css';
+
+
+// to display the navigation bar
+export default function Navbar() {
+    return (
+        <>
+            <div className='spacer layer' style={{ width: '100%', height: '14vh' }}>
+                <div style={{ fontWeight: 'bold', fontSize: '25px', marginLeft: '2%', paddingTop: '1vh', color: 'white' }}>
+                    HTML to JSX
+                </div>
+            </div>
+        </>
+    )
+}
+
