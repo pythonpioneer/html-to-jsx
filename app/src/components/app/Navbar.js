13c0430b1dc97c25c6c0d48cd749ade75803e645
Hritik <pypi.pioneer@gmail.com>
2023-12-02 02:28:31 +0530

    title added

--- app/src/components/app/Navbar.js
+++ app/src/components/app/Navbar.js
@@ -6,8 +6,9 @@ export default function Navbar() {
     return (
         <>
             <nav className='spacer layer' style={{ width: '100%', height: '14vh' }}>
-                <div style={{ fontWeight: 'bold', fontSize: '25px', marginLeft: '2%', paddingTop: '1vh', color: 'white' }}>
+                <div style={{ fontWeight: 'bold', fontSize: '25px', marginLeft: '2%', paddingTop: '1vh', color: 'beige' }}>
                     HTML to JSX
+                    <div style={{ float: 'right', fontSize: '12px', color: 'black', marginRight: '2%' }}>@pythonpioneer</div>
                 </div>
             </nav>
         </>
