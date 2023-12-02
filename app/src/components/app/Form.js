2a98683c11199d63e1d3ee1fba9ef6eaa9ce56b8
Hritik <pypi.pioneer@gmail.com>
2023-12-02 16:52:03 +0530

    function added to convert

--- app/src/components/app/Form.js
+++ app/src/components/app/Form.js
@@ -1,4 +1,6 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react';
+const ReactDOMServer = require('react-dom/server');
+const HtmlToReactParser = require('html-to-react').Parser;
 
 
 // to display the form
@@ -6,6 +8,7 @@ export default function Form() {
 
     // to store the form text
     const htmlTextRef = useRef('');
+    const [jsxText, setJsxText] = useState('');
     
     // now fetch the form data
     const fetchFormData = () => {
@@ -17,7 +20,7 @@ export default function Form() {
             <div className='container mt-2'>
                 <div>
                     <label style={{ fontSize: '16px', fontWeight: 'bold' }}>Your HTML</label>
-                    <textarea ref={htmlTextRef} style={{ width: '100%', height: '30vh', backgroundColor: 'transparent' }} defaultValue="Write your HTML here"></textarea>
+                    <textarea ref={htmlTextRef} style={{ width: '100%', height: '30vh', backgroundColor: 'transparent' }} defaultValue='<input class="demo" for="demo">'></textarea>
                 </div>
 
                 <div className='' style={{ justifyContent: 'center', display: 'flex' }}>
@@ -30,5 +33,21 @@ export default function Form() {
                 </div>
             </div>
         </>
-    )
+    );
 }
+
+// to convert the html text into jsx
+function htmlToJSX(html) {
+
+    // Replace self-closing tags in HTML with equivalent JSX tags
+    const htmlToReactParser = new HtmlToReactParser();
+    const reactElement = htmlToReactParser.parse(html);
+    const reactHtml = ReactDOMServer.renderToStaticMarkup(reactElement);
+     
+    // Replace class attributes with className in JSX
+    const jsxWithClassName = reactHtml.replace(/class=/g, 'className=');
+
+    // Replace for attributes with htmlFor in JSX
+    const jsxWithHtmlFor = jsxWithClassName.replace(/for=/g, 'htmlFor=');  
+    return jsxWithHtmlFor; 
+}  
